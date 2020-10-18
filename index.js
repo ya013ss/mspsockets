b69d587ccda35f5f658f796b2fe07e2ee91e4127
ya013s <66035900+ya013ss@users.noreply.github.com>
2020-10-18 22:06:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const WSPort = 10843;
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: WSPort });
  
+console.log(`Ready.`);
+
 wss.on(`connection`, (ws) => {
+    console.log(`Oohh! A new connection?`);
     ws.on(`message`, (message) => {
         console.log(`RECIEVED: ${message}`);
     });
